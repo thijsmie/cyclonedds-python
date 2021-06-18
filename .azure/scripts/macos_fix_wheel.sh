9d83747d96b11c8b1fe42a0bafa96b908d489a28
Thijs Miedema <t.miedema@live.nl>
2021-06-18 11:04:32 +0200

    Move fix wheel macos to script

---
+++ .azure/scripts/macos_fix_wheel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e -x
+
+DESTINATION=$1
+WHEEL=$2
+CYCLONEDDS_HOME=$3
+DELOCATE_ARCHS=$4
+
+# Unpack the wheel
+wheel unpack -d temp $WHEEL
+
+# Get the wheel directory
+files=(temp/*)
+UNWHEEL="${files[0]}"
+files=($UNWHEEL/ddspy.*)
+DYLIB="${files[0]}"
+
+
+# Strip out the rpath
+
+echo $(otool -L $DYLIB)
+install_name_tool -change @rpath/libddsc.0.dylib $CYCLONEDDS_HOME/lib/libddsc.dylib $DYLIB
+echo $(otool -L $DYLIB)
+
+rm $WHEEL
+wheel pack -d $(dirname $WHEEL) $UNWHEEL
+rm -rf temp
+
+delocate-listdeps $WHEEL
+delocate-wheel -v --require-archs $DELOCATE_ARCHS -w $DESTINATION $WHEEL
