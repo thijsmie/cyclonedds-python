a6229f5a3abd8cdb7702e1ad2f19d5a22597bde6
Thijs Miedema <thijs.miedema@adlinktech.com>
2021-07-20 16:02:59 +0200

    Release pipeline fixes

--- src/cyclonedds/setup.py
+++ src/cyclonedds/setup.py
@@ -12,9 +12,16 @@
 """
 
 import os
-import logging
 from setuptools import find_packages
+
 from skbuild import setup
+
+
+# Because Scikit-Build sets -G on the cmake command CMAKE_GENERATOR_PLATFORM is
+# ignored. By converting it into a command line argument it can be passed through.
+cmake_args = []
+if "CMAKE_GENERATOR_PLATFORM" in os.environ:
+    cmake_args.append(f"-A {os.environ['CMAKE_GENERATOR_PLATFORM']}")
 
 setup(
     name='cyclonedds',
@@ -37,7 +44,8 @@ setup(
         "Operating System :: OS Independent"
     ],
     install_requires=["pycdr"],
-    packages=find_packages('.', exclude=("tests", "examples")),
+    packages=find_packages('.', exclude=("tests", "examples", "tools")),
+    cmake_args=cmake_args,
     scripts=['tools/ddsls.py'],
     python_requires='>=3.6'
 )
